8f8ee84ebab3e9405dc17ffe7374fa8e06ff6b66
Harsh Dave <harshdave161@gmail.com>
2019-08-16 15:52:04 +0530

    ver 4

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const logo=document.querySelectorAll('#deve path');
-for(let i=0;i<logo.length;i++){
+const logo = document.querySelectorAll('#deve path');
+for (let i = 0; i < logo.length; i++) {
     console.log(`letter ${i} of ${logo[i].getTotalLength()}`);
 }
